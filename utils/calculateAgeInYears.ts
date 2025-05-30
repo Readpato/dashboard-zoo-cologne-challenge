b9d9c43be743f76984b0a21cead8e8874c440c9d
Patrick Raedler <code@readpato.dev>
2025-05-30 18:31:18 +0200

    refactor(utils/calculateAgeInYears): adjust function to return a minimum of one for the age

--- utils/calculateAgeInYears.ts
+++ utils/calculateAgeInYears.ts
@@ -1,6 +1,6 @@
 export default (birthdate: Date) => {
   const today = new Date()
   const differenceInMilliseconds = today.getTime() - birthdate.getTime()
-
-  return Math.round(differenceInMilliseconds / (1000 * 60 * 60 * 24 * 365))
+  const ageInYears = Math.ceil(differenceInMilliseconds / (1000 * 60 * 60 * 24 * 365))
+  return Math.max(1, ageInYears)
 }
