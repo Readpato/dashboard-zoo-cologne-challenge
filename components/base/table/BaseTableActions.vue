2d3791f4639dcd4d167cfc262c51eda7d9eb3dde
Patrick Raedler <code@readpato.dev>
2025-06-01 18:16:11 +0200

    feat(components/base): create BaseTableActions component

---
+++ components/base/table/BaseTableActions.vue
@@ -0,0 +1,20 @@
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <Button
+          variant="ghost"
+          class="size-6 p-0 cursor-pointer"
+        >
+          <BaseIcon
+            name="info"
+            class="text-xs text-muted-foreground"
+          />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>See more information</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
