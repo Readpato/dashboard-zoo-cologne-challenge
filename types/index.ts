fd60a97250cc39e0ff8cc0fcc053b4210f518ec6
Patrick Raedler <code@readpato.dev>
2025-06-01 12:02:30 +0200

    refactor(types): update animal type, sort alphabetically

--- types/index.ts
+++ types/index.ts
@@ -2,11 +2,12 @@ export type Fruit = 'banana' | 'apple' | 'cherry'
 export type Gender = 'male' | 'female'
 
 export type Animal = {
-  name: string
-  species: string
-  gender: Gender
   birthdate: Date
   favouriteFruit: Fruit
+  gender: Gender
   height: number
+  id: string
+  name: string
+  species: string
   weight: number
 }
