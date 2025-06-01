13319d1fe00c4aec4c7d73d9308e7316ba89181d
Patrick Raedler <code@readpato.dev>
2025-06-01 17:40:10 +0200

    feat(pages): create main view and animal with dynamic routing

---
+++ pages/animals/[id].vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <NuxtLink to="/">
+      Back
+    </NuxtLink>
+    <div>I'm animal with id: {{ $route.params.id }}</div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { animals } = useAnimals()
+</script>
+
+<template>
+  <div class="w-full">
+    <h2 class="text-lg lg:text-xl">
+      Our (lovely) animals
+    </h2>
+    <TheAnimalTable
+      v-if="animals"
+      :animals
+    />
+  </div>
+</template>
