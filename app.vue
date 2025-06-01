926b5880d87a73593147f631caeb5616073542f0
Patrick Raedler <code@readpato.dev>
2025-06-01 17:40:28 +0200

    refactor(app.vue): implement NuxtPage component, clean up unnecesary ones

--- app.vue
+++ app.vue
@@ -1,16 +1,6 @@
-<script setup lang="ts">
-const { animals } = useAnimals()
-</script>
-
 <template>
   <AppHeader />
   <main class="container mx-auto p-6">
-    <h2 class="text-lg lg:text-xl">
-      Our (lovely) animals
-    </h2>
-    <TheAnimalTable
-      v-if="animals"
-      :animals
-    />
+    <NuxtPage />
   </main>
 </template>
