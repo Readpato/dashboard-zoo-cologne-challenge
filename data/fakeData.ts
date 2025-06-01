4b656b52ccc4ae6ced9587d82a737361738231c9
Patrick Raedler <code@readpato.dev>
2025-06-01 12:01:19 +0200

    chore(fakeData): implement uuid to animal

--- data/fakeData.ts
+++ data/fakeData.ts
@@ -2,12 +2,13 @@ import type { Animal, Fruit, Gender } from '~/types'
 import { faker } from '@faker-js/faker'
 
 export const AARON: Animal = {
-  name: 'Aaron',
-  species: 'dog',
-  gender: 'male',
   birthdate: new Date(),
   favouriteFruit: 'cherry',
+  gender: 'male',
   height: 150,
+  id: faker.string.uuid(),
+  name: 'Aaron',
+  species: 'dog',
   weight: 150,
 }
 
@@ -16,20 +17,21 @@ export const fakeAnimal = (): Animal => {
   const favouriteFruit = faker.helpers.arrayElement(['banana', 'apple', 'cherry']) satisfies Fruit
 
   return {
-    name: faker.person.firstName(gender),
-    species: faker.animal.type(),
     birthdate: faker.date.past({ years: 25 }),
-    weight: faker.number.float({
-      min: 20,
-      max: 250,
-      multipleOf: 0.1,
-    }),
+    favouriteFruit,
+    gender,
     height: faker.number.float({
       min: 0.25,
       max: 4,
       multipleOf: 0.01,
     }),
-    gender,
-    favouriteFruit,
+    id: faker.string.uuid(),
+    name: faker.person.firstName(gender),
+    species: faker.animal.type(),
+    weight: faker.number.float({
+      min: 20,
+      max: 250,
+      multipleOf: 0.1,
+    }),
   }
 }
