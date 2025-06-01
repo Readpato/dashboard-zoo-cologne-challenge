990aec3403be3ff4b4214c6d7ce0c084d2b1f0ee
Patrick Raedler <code@readpato.dev>
2025-06-01 16:24:49 +0200

    feat(components/app): create header component

---
+++ components/app/AppHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="flex justify-start items-center space-x-2 py-2 px-4 border-b">
+    <BaseIcon
+      name="bird"
+      class="text-lg lg:text-2xl"
+    />
+    <h1 class="text-lg lg:text-2xl">
+      Cologne Zoo Dashboard
+    </h1>
+  </header>
+</template>
