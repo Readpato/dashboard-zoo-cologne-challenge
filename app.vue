faf8e0cd1ea23d6d08e4e21ce843ab143a569540
Patrick Raedler <code@readpato.dev>
2025-06-03 11:49:58 +0200

    style(App.vue): make header sticky

--- app.vue
+++ app.vue
@@ -2,11 +2,20 @@
 useHead({
   titleTemplate: '%s | Cologne Zoo Dashboard',
 })
+
+const headerRef = useTemplateRef('header')
+const { height } = useElementBounding(headerRef)
 </script>
 
 <template>
-  <AppHeader />
-  <main class="container mx-auto p-6">
+  <AppHeader
+    ref="header"
+    class="sticky top-0 bg-background z-50"
+  />
+  <main
+    class="container mx-auto p-6"
+    :style="{ minHeight: `calc(100vh - ${height}px)` }"
+  >
     <NuxtPage />
   </main>
 </template>
